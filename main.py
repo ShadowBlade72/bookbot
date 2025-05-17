1461a98b75b301ec0fb5e5e7514742d02f9c8d73
ShadowBlade72 <UnderCoverSPI101@gmail.com>
2025-05-17 09:35:49 -0400

    Updated to match format for bookbot

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 #!/bin/python3
-from stats import num_of_words
-from stats import num_of_char
-from stats import pretty_num_of_char
+from stats import num_of_words, num_of_char, pretty_num_of_char
 
-def sort_on(dict):
-    return dict["num"]
+def main():
+    num_words = num_of_words(get_book_text("books/frankenstein.txt"))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    num_char = num_of_char(get_book_text("books/frankenstein.txt"))
+#    print(f"{num_char}")
+
+    print("--------- Character Count -------")
+    pretty_num_char = pretty_num_of_char(get_book_text("books/frankenstein.txt"))
+    for char in pretty_num_char:
+        print(f'{char["char"]}: {char["num"]}')
+    print("============= END ===============") 
 
 def get_book_text(filepath):
     with open(filepath, encoding="utf8") as f:
         return f.read()
-
-def main():
-    num_words = num_of_words(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-
-    num_char = num_of_char(get_book_text("books/frankenstein.txt"))
-    print(f"{num_char}")
-
-    pretty_num_char = pretty_num_of_char(get_book_text("books/frankenstein.txt"))
-    for char in pretty_num_char:
-#        if char["char"].isalpha():
-#        print(f"{char}")
-        print(f'{char["char"]}: {char["num"]}')
-#        pass
 
 main()
