9e803c86c00dcc0d568b14bbb9467d9150ba657a
ShadowBlade72 <UnderCoverSPI101@gmail.com>
2025-05-17 09:45:31 -0400

    Revised code for readability.

--- main.py
+++ main.py
@@ -2,23 +2,24 @@
 from stats import num_of_words, num_of_char, pretty_num_of_char
 
 def main():
-    num_words = num_of_words(get_book_text("books/frankenstein.txt"))
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-
-    num_char = num_of_char(get_book_text("books/frankenstein.txt"))
-#    print(f"{num_char}")
-
-    print("--------- Character Count -------")
-    pretty_num_char = pretty_num_of_char(get_book_text("books/frankenstein.txt"))
-    for char in pretty_num_char:
-        print(f'{char["char"]}: {char["num"]}')
-    print("============= END ===============") 
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = num_of_words(text)
+    pretty_num_char = pretty_num_of_char(text)
+    print_report(book_path, num_words, pretty_num_char)
 
 def get_book_text(filepath):
     with open(filepath, encoding="utf8") as f:
         return f.read()
+
+def print_report(book_path, num_words, pretty_num_char):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in pretty_num_char:
+        print(f'{char["char"]}: {char["num"]}')
+    print("============= END ===============") 
 
 main()
